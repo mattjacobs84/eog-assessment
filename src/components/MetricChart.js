4ab0eeba68a9c49c4f2117bf09d0a0da87bf8b61
EC2 Default User <ec2-user@ip-172-31-35-34.us-east-2.compute.internal>
2019-12-03 04:32:52 +0000

    Remove Recharts, working live data with Nivo charts

--- src/components/MetricChart.js
+++ src/components/MetricChart.js
@@ -1,29 +1,88 @@
 import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
-import {
-  LineChart, Line, XAxis, YAxis, Tooltip, Legend,
-} from 'recharts';
+import { ResponsiveLine } from '@nivo/line';
 
 const SpecificChart = ({data, metricList}) => {
+  
   const eachChart = useMemo(() => {
+    const metricTitle = metricList.find(item => item.metric === data[0].metric).name;
+    const updatedData = data.map(item => {
+      const time = new Date(item.at);
+      return {"x": `${time.getMinutes()}:${time.getSeconds()}`, "y": item.value};
+    });
+    const newData = [
+      {
+        "id": `${metricTitle}`,
+          "data": updatedData
+      }
+    ];
     return (
-        <div>
-          {metricList.find(item => item.metric === data[0].metric).name}
-          <LineChart
-              width={500}
-              height={300}
-              data={data}
-              margin={{
-                top: 45, right: 30, left: 20, bottom: 5,
-              }}
-          >
-              <XAxis dataKey="at" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
-          </LineChart>
+        <div style={{height: '400px', width: '50%'}}>
+          {metricTitle}
+          <ResponsiveLine
+                  data={newData}
+                  margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                  xScale={{
+                      type: 'time',
+                      format: '%M:%S'
+                  }}
+                  xFormat="time:%M:%S"
+                  yScale={{ type: 'linear', max: 'auto' }}
+                  axisTop={null}
+                  axisRight={null}
+                  axisBottom={{
+                    format: '%M:%S',
+                    tickValues: 'every 1 minute',
+                    legend: 'time scale',
+                    legendOffset: -12,
+                  }}
+                  axisLeft={{
+                      orient: 'left',
+                      tickSize: 5,
+                      tickPadding: 5,
+                      tickRotation: 0,
+                      legend: `${data[0].unit}`,
+                      legendOffset: -40,
+                      legendPosition: 'middle'
+                  }}
+                  colors={{ scheme: 'nivo' }}
+                  enablePoints={false}
+                  pointSize={10}
+                  pointColor={{ theme: 'background' }}
+                  pointBorderWidth={2}
+                  pointBorderColor={{ from: 'serieColor' }}
+                  pointLabel="y"
+                  pointLabelYOffset={-12}
+                  enableCrosshair={false}
+                  useMesh={true}
+                  legends={[
+                      {
+                          anchor: 'bottom-right',
+                          direction: 'column',
+                          justify: false,
+                          translateX: 100,
+                          translateY: 0,
+                          itemsSpacing: 0,
+                          itemDirection: 'left-to-right',
+                          itemWidth: 80,
+                          itemHeight: 20,
+                          itemOpacity: 0.75,
+                          symbolSize: 12,
+                          symbolShape: 'circle',
+                          symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                          effects: [
+                              {
+                                  on: 'hover',
+                                  style: {
+                                      itemBackground: 'rgba(0, 0, 0, .03)',
+                                      itemOpacity: 1
+                                  }
+                              }
+                          ]
+                      }
+                  ]}
+              />
         </div>
     );
   }, [data]);
